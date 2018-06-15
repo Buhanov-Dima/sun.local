0d187d80b875f4035442179ca522eb8e0f717c2a
Buhanov-Dima <35217770+Buhanov-Dima@users.noreply.github.com>
2018-06-15 18:23:40 +0500

    +

--- js/main.js
+++ js/main.js
@@ -51,4 +51,10 @@ $(window).scroll(function(){
     else {
         $('.wrap-header').removeClass('fixed');
     };
+    if ($(window).scrollTop() > 30) {
+        $('.bg-white-color').addClass('shadow-cl');
+    }
+    else {
+        $('.bg-white-color').removeClass('shadow-cl');
+    };
 });
